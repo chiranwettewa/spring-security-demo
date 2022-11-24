b36a48740d1fd0aef14194a47f253f0d6caf06e3
Chiran_105626 <Chiran_105626@105626-002L>
2022-11-24 18:03:47 +0530

    Oauth 2 init

---
+++ Oauth-authorization-server/src/main/java/com/chiran/oauthserver/entity/User.java
@@ -0,0 +1,30 @@
+package com.chiran.oauthserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Email
+    private String email;
+    @Column(length=60)
+    private String password;
+    private String role;
+    private boolean enabled = false;
+
+}
---
+++ Oauth-authorization-server/src/test/java/com/chiran/oauthserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiran.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
